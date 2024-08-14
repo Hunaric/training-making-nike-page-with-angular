41f123398352ae58a48850b20f52b1025da5a4e0
NATH FANG <nathanaelfangnon@gmail.com>
2024-08-14 15:48:03 +0100

    End with the nav hamburger button option

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -7,7 +7,7 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <header class="sm:px-16 px-8 py-8 absolute z-10 w-full">
+    <header class="sm:px-16 px-8 py-8 relative w-full bg-white">
       <nav class="flex justify-between items-center max-container">
         <a href="/">
           <img src="assets/images/header-logo.svg" alt="Logo" width="130px" height="29">
@@ -18,9 +18,14 @@ import { CommonModule } from '@angular/common';
           </li>
         </ul>
         <div class="hidden max-lg:block">
-          <img src="{{hamburger}}" alt="Hamburger" width="25px" height="25px">
+          <img src="{{hamburger}}" alt="Hamburger" width="25px" height="25px" (click)="toggleMenu()" class="cursor-pointer">
         </div>
       </nav>
+      <ul *ngIf="isMenuOpen" class="lg:hidden flex flex-col items-center mt-4 space-y-4 text-center py-4 bg-white shadow-lg transition-transform duration-700 ease-in-out transform z-20 w-full absolute left-0 top-full">
+        <li *ngFor="let link of navLinks">
+          <a href="{{ link.href }}" class="font-montserrat leading-normal text-lg text-gray-400">{{ link.label }}</a>
+        </li>
+      </ul>
     </header>
   `,
   styleUrl: './nav.component.css'
@@ -28,9 +33,10 @@ import { CommonModule } from '@angular/common';
 export class NavComponent {
   navLinks = navLinks;
   hamburger = hamburger;
+  isMenuOpen = false;
 
-  constructor() {
-    console.log(navLinks);
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
   }
 
 }
